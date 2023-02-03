c803d7cf1c7d940fca4bd80a2b150ee324e94374
ksjmgrkks <ksjmgrkks@naver.com>
2023-02-03 18:37:29 +0900

    [chore] gradle 설정

---
+++ Whereareyou/app/src/main/java/com/android/whereareyou/WhereAreYouApplication.kt
@@ -0,0 +1,7 @@
+package com.android.whereareyou
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WhereAreYouApplication : Application()
--- Whereareyou/app/src/main/java/com/android/whereareyou/ui/MainActivity.kt
+++ Whereareyou/app/src/main/java/com/android/whereareyou/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.android.whereareyou
+package com.android.whereareyou.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.whereareyou.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
