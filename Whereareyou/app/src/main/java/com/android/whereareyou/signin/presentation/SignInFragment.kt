f997047b21a534afb85c09bc022d05447fae513d
김규성 <ksjmgrkks@naver.com>
2023-02-22 11:08:30 +0900

    [refactor] 리팩토링 작업 - kakaoLogin 함수 rx부분 분리 - AutoDispose 적용 - SignInViewModel 제거

--- Whereareyou/app/src/main/java/com/android/whereareyou/signin/presentation/SignInFragment.kt
+++ Whereareyou/app/src/main/java/com/android/whereareyou/signin/presentation/SignInFragment.kt
@@ -1,41 +1,35 @@
 package com.android.whereareyou.signin.presentation
 
-import android.app.Activity
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
 import com.android.whereareyou.R
 import com.android.whereareyou.WhereAreYouApplication
 import com.android.whereareyou.core.BaseFragment
 import com.android.whereareyou.core.data.api.interceptor.log.Logger
 import com.android.whereareyou.core.util.moveScreen
 import com.android.whereareyou.core.util.preference.PreferenceConstants
-import com.android.whereareyou.databinding.FragmentSignInBinding
-import com.kakao.sdk.common.util.Utility
-import com.kakao.sdk.talk.TalkApiClient
-import com.kakao.sdk.user.UserApiClient
-import com.kakao.sdk.user.rx
-import dagger.hilt.android.AndroidEntryPoint
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.addTo
-import io.reactivex.schedulers.Schedulers
-import com.android.whereareyou.core.util.preference.PreferenceHelper.get
 import com.android.whereareyou.core.util.preference.PreferenceHelper.set
 import com.android.whereareyou.core.util.showToast
+import com.android.whereareyou.databinding.FragmentSignInBinding
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
-import io.reactivex.Single
-import io.reactivex.disposables.CompositeDisposable
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.user.rx
+import com.uber.autodispose.ScopeProvider
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
+import com.uber.autodispose.autoDispose
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 
 @AndroidEntryPoint
 class SignInFragment : BaseFragment() {
-    private val disposables = CompositeDisposable()
+    private val scopeProvider = ScopeProvider.UNBOUND
     private var _binding: FragmentSignInBinding? = null
     private val binding
         get() = requireNotNull(_binding)
@@ -59,60 +53,61 @@ class SignInFragment : BaseFragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        disposables.clear()
         _binding = null
     }
 
     private fun setupUI() {
         activityViewModel.settingUI(false)
         binding.imageViewKakaoLogin.setOnClickListener {
-            kakaoLogin(requireContext())
+            requireContext().run {
+                // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오 설치 유도 토스트 메시지
+                if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                    kakaoLogin(this)
+                } else {
+                    showToast(this, getString(R.string.fragment_check_in_install_kakao))
+                }
+            }
         }
     }
 
-    private fun kakaoLogin(context: Context) {
-        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오 설치 유도 토스트 메시지
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            disposables.add(
-                UserApiClient.rx.loginWithKakaoTalk(context)
+    private fun kakaoLogin(context: Context) =
+        UserApiClient.rx.loginWithKakaoTalk(context)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnError {
+                Logger.e(message = "로그인 실패 ${it.message}")
+            }
+            .flatMap {
+                Logger.i("로그인 성공 ${it.accessToken}")
+                //유저 정보 요청
+                UserApiClient.rx.me()
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .doOnError { error ->
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                            Logger.e(message = "로그인 실패 ${error.message}")
-                        } else {
-                            // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정 연결 요청 메시지
-                            showToast(
-                                requireContext(),
-                                getString(R.string.fragment_check_in_link_kakao_account)
-                            )
-                        }
-                    }
-                    .flatMap {
-                        Logger.i("로그인 성공 ${it.accessToken}")
-                        //유저 정보 요청
-                        UserApiClient.rx.me()
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                    }
-                    .subscribe({ user ->
-                        Logger.i(
-                            "사용자 정보 요청 성공" +
-                                    "\n회원 번호: ${user.id}" +
-                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}"
-                        )
-                        if (user.id != null && user.kakaoAccount?.profile?.nickname != null) {
-                            WhereAreYouApplication.prefs[PreferenceConstants.USER_ID] = user.id
-                            WhereAreYouApplication.prefs[PreferenceConstants.NICKNAME] =
-                                user.kakaoAccount?.profile?.nickname
-                            moveScreen(R.id.action_sign_in_to_weekly_schedule)
-                        }
-                    }, { error ->
-                        Logger.e(message = "사용자 정보 요청 실패 ${error.localizedMessage}")
-                    }).addTo(disposables)
-            )
-        } else {
-            showToast(requireContext(), getString(R.string.fragment_check_in_install_kakao))
-        }
-    }
+            }
+            .autoDispose(scopeProvider)
+            .subscribe({ user ->
+                Logger.i(
+                    "사용자 정보 요청 성공" +
+                            "\n회원 번호: ${user.id}" +
+                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}"
+                )
+                if (user.id != null && user.kakaoAccount?.profile?.nickname != null) {
+                    WhereAreYouApplication.prefs[PreferenceConstants.USER_ID] = user.id
+                    WhereAreYouApplication.prefs[PreferenceConstants.NICKNAME] =
+                        user.kakaoAccount?.profile?.nickname
+                    moveScreen(R.id.action_sign_in_to_weekly_schedule)
+                }
+            }, { error ->
+                if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    Logger.e(message = "로그인 실패 ${error.message}")
+                } else {
+                    Logger.e(message = "에러 로그: ${error.localizedMessage}")
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정 연결 요청 메시지
+                    showToast(
+                        requireContext(),
+                        getString(R.string.fragment_check_in_link_kakao_account)
+                    )
+                }
+
+            })
 }
