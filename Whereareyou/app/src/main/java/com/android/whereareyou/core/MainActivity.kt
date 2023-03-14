79cc150a0491d5fea0d95608566182aff2b25507
김규성 <ksjmgrkks@gmail.com>
2023-03-14 14:44:42 +0900

    [feat] SplashScreen 적용 참고1 : https://developer.android.com/develop/ui/views/launch/splash-screen 참고2 : https://developer.android.com/develop/ui/views/launch/splash-screen/migrate

--- Whereareyou/app/src/main/java/com/android/whereareyou/core/MainActivity.kt
+++ Whereareyou/app/src/main/java/com/android/whereareyou/core/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.android.whereareyou.core
 
+import android.animation.ObjectAnimator
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.android.whereareyou.R
 import com.android.whereareyou.core.data.api.interceptor.log.Logger
 import com.android.whereareyou.core.util.getForegroundFragment
@@ -14,6 +19,10 @@ import com.android.whereareyou.schedule.presentation.calendar.CalendarScheduleFr
 import com.android.whereareyou.schedule.presentation.daily.DailyScheduleFragment
 import com.android.whereareyou.schedule.presentation.weekly.WeeklyScheduleFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -23,7 +32,14 @@ class MainActivity : AppCompatActivity() {
         get() = requireNotNull(_binding)
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+//        splashScreen.setKeepOnScreenCondition{ true }
+//        lifecycleScope.launch {
+//            delay(1000)
+//            splashScreen.setKeepOnScreenCondition{ false }
+//        }
+
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
     }
